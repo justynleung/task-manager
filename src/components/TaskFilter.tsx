171041ed207869fa2b207de295e1ea9321b1b9b0
justynleung <justyn@Justyns-MacBook-Pro.local>
2023-08-03 17:57:20 -0400

    filter method check truthy too

--- src/components/TaskFilter.tsx
+++ src/components/TaskFilter.tsx
@@ -7,9 +7,9 @@ interface Props {
 }
 
 export default function TaskFilter({ tasks, setTasks, filter }: Props) {
-    const filteredTasks = [...tasks].filter((item) => item.category === filter)
+    const filteredTasks = tasks && [...tasks].filter((item) => item.category === filter)
     const removeItem = (id: string) => {
-        const updatedList = tasks.filter((item) => id !== item.id)
+        const updatedList = tasks && tasks.filter((item) => id !== item.id)
         setTasks(updatedList)
     }
     const tasksList = filteredTasks && filteredTasks.map((item, i) => {
